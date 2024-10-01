7cbccc9cdaf77dc0fcbff8101dc50f81d7623af7
DungVPN <dungbgbg12tt@gmail.com>
2024-10-01 13:08:11 +0700

    ss7

---
+++ ss7-vue/src/components/ParentComponent.vue
@@ -0,0 +1,23 @@
+<script setup>
+import ChildrenComponet from './ChildrenComponet.vue'
+const userName='NGUYEN VAN A'
+const age=18
+
+const arr = {
+    id:1,
+    name:'DUNG',
+    age:19
+}
+</script>
+
+<template>
+  <div>Cha
+    <h3>{{userName}}</h3>
+    <p>---------------------</p>
+    <ChildrenComponet :arr1='arr'></ChildrenComponet>
+  </div>
+</template>
+
+<style>
+
+</style>
